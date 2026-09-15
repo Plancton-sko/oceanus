a21051c4a2b081c54b57d2fccc8dd301d7377e9d
Plancton-Sko <joaopaulo.skodowski@gmail.com>
2026-09-15 17:12:58 -0300

    Wallpapers

--- hosts/oceanus/home.nix
+++ hosts/oceanus/home.nix
@@ -59,5 +59,6 @@
     "rmpc".source                             = ../../apps/rmpc;
     "matugen".source                          = ../../apps/matugen;
     "desktop/themes".source                   = ../../desktop/themes;
+    "wallpapers".source                       = ../../assets/wallpapers;
   };
 }
